f2a0495d43b1b5f0650a2ce3b97b0e94d08a3861
Alsesd <mr.alsesd@gmail.com>
2025-08-20 00:20:57 +0300

    Enhance dark theme configuration and package management in home.nix

--- home.nix
+++ home.nix
@@ -1,43 +1,216 @@
-{ config, pkgs, ... }:
-
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
+  config,
+  pkgs,
+  ...
+}: {
   home.username = "alsesd";
   home.homeDirectory = "/home/alsesd";
+  home.stateVersion = "25.05";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+  home.packages = with pkgs; [
+    pkgs.hello
+    (pkgs.writeShellScriptBin "my-hello" ''
+      echo "Hello, ${config.home.username}!"
+    '')
+    papirus-icon-theme # Popular dark icon theme
+    adwaita-icon-theme # GNOME's default icons
+    gnome-themes-extra # Includes Adwaita cursor
+    arc-theme # Arc dark theme
+    materia-theme # Material design theme
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
+  # GTK theme configuration (affects most GUI applications)
+  gtk = {
+    enable = true;
+
+    # Main theme
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome-themes-extra;
+    };
+
+    # Icon theme
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+
+    # Cursor theme
+    cursorTheme = {
+      name = "Adwaita";
+      package = pkgs.gnome-themes-extra;
+      size = 24;
+    };
+
+    # Font configuration
+    font = {
+      name = "Inter"; # Modern font, or use "Sans" for system default
+      size = 11;
+    };
+
+    # GTK3 specific settings
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+      gtk-button-images = 1;
+      gtk-menu-images = 1;
+      gtk-enable-event-sounds = 1;
+      gtk-enable-input-feedback-sounds = 1;
+      gtk-xft-antialias = 1;
+      gtk-xft-hinting = 1;
+      gtk-xft-hintstyle = "hintfull";
+      gtk-xft-rgba = "rgb";
+    };
+
+    # GTK4 specific settings
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+
+    # GTK2 settings for legacy applications
+    gtk2.extraConfig = ''
+      gtk-application-prefer-dark-theme = 1
+      gtk-theme-name = "Adwaita-dark"
+      gtk-icon-theme-name = "Papirus-Dark"
+      gtk-cursor-theme-name = "Adwaita"
+      gtk-cursor-theme-size = 24
+      gtk-font-name = "Inter 11"
+    '';
+  };
+
+  # Qt theme configuration
+  qt = {
+    enable = true;
+    platformTheme.name = "gtk3"; # Make Qt follow GTK theme
+    style.name = "adwaita-dark"; # Qt style
+  };
+
+  # Environment variables for consistent theming
+  home.sessionVariables = {
+    # GTK theme
+    GTK_THEME = "Adwaita-dark";
+
+    # Qt theming
+    QT_QPA_PLATFORMTHEME = "gtk3";
+    QT_STYLE_OVERRIDE = "adwaita-dark";
+
+    # Cursor theme
+    XCURSOR_THEME = "Adwaita";
+    XCURSOR_SIZE = "24";
+
+    # Force dark mode for applications that support it
+    GTK_APPLICATION_PREFER_DARK_THEME = "1";
+  };
+
+  # Application-specific dark theme configurations
+  programs = {
+    # VS Code dark theme
+    vscode = {
+      enable = true;
+      userSettings = {
+        "workbench.colorTheme" = "Default Dark Modern";
+        "workbench.iconTheme" = "vs-minimal";
+        "editor.theme" = "Default Dark Modern";
+      };
+    };
+
+    # Firefox dark theme
+    firefox = {
+      enable = true;
+      profiles.default = {
+        settings = {
+          # Force dark theme
+          "ui.systemUsesDarkTheme" = 1;
+          "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+
+          # Dark private browsing
+          "browser.theme.dark-private-browsing" = true;
+
+          # Dark developer tools
+          "devtools.theme" = "dark";
+
+          # Prefer dark content
+          "layout.css.prefers-color-scheme.content-override" = 0;
+        };
+
+        userChrome = ''
+          /* Dark scrollbars */
+          * {
+            scrollbar-color: #484848 #2b2b2b;
+          }
+        '';
+      };
+    };
+
+    # Kitty terminal dark theme
+    kitty = {
+      enable = true;
+      theme = "Tokyo Night"; # Popular dark theme
+      settings = {
+        background_opacity = "0.9";
+        font_family = "FiraCode Nerd Font Mono";
+        font_size = 12;
+      };
+    };
+
+    # Git with dark theme for delta (diff viewer)
+    git = {
+      enable = true;
+      delta = {
+        enable = true;
+        options = {
+          features = "decorations";
+          syntax-theme = "Monokai Extended Dark";
+          plus-style = "syntax #003800";
+          minus-style = "syntax #3f0001";
+        };
+      };
+    };
+
+    # Rofi dark theme
+    rofi = {
+      enable = true;
+      theme = "Arc-Dark";
+    };
+  };
+
+  # XDG configuration files for applications that don't have Home Manager modules
+  xdg.configFile = {
+    # Discord dark theme (BetterDiscord theme)
+    "BetterDiscord/themes/dark.theme.css".text = ''
+      /* Dark theme customizations */
+      :root {
+        --background-primary: #36393f;
+        --background-secondary: #2f3136;
+        --background-tertiary: #202225;
+      }
+    '';
+
+    # Thunar (file manager) dark theme
+    "gtk-3.0/bookmarks".text = ''
+      # Dark themed bookmarks
+    '';
+  };
+
+  # Services for theme consistency
+  services = {
+    # XSettings daemon for theme coordination
+    xsettingsd = {
+      enable = true;
+      settings = {
+        "Net/ThemeName" = "Adwaita-dark";
+        "Net/IconThemeName" = "Papirus-Dark";
+        "Gtk/CursorThemeName" = "Adwaita";
+        "Gtk/CursorThemeSize" = 24;
+        "Net/SoundThemeName" = "freedesktop";
+        "Net/EnableEventSounds" = 1;
+        "Net/EnableInputFeedbackSounds" = 1;
+      };
+    };
+  };
+
+  # Fonts for better dark theme experience
+  fonts.fontconfig.enable = true;
+
   home.file = {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
