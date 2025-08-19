8562d0127b440dd0732f433ff16c020c251f8628
Alsesd <mr.alsesd@gmail.com>
2025-08-19 23:18:36 +0300

    Fix nixpkgs URL format in flake.nix for consistency

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My favourite NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
