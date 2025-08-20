8f168cd746fc5c369018fc0f4264dce5ad81f459
Alsesd <mr.alsesd@gmail.com>
2025-08-20 13:14:24 +0300

    1

--- home-module/nix-colors.nix
+++ home-module/nix-colors.nix
@@ -10,24 +10,5 @@
 
   colorScheme = nix-colors.colorSchemes.dracula;
 
-  programs = {
-    kitty = {
-      enable = true;
-      settings = {
-        foreground = "#${config.colorScheme.palette.base05}";
-        background = "#${config.colorScheme.palette.base00}";
-        # ...
-      };
-    };
-    qutebrowser = {
-      enable = true;
-      colors = {
-        # Becomes either 'dark' or 'light', based on your colors!
-        webppage.preferred_color_scheme = "${config.colorScheme.variant}";
-        tabs.bar.bg = "#${config.colorScheme.palette.base00}";
-        keyhint.fg = "#${config.colorScheme.palette.base05}";
-        # ...
-      };
-    };
-  };
+  programs.enable = true;
 }
