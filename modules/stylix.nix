0133df9ea3b756541cd83280dbdadca0d08858f6
Alsesd <mr.alsesd@gmail.com>
2025-08-20 16:35:01 +0300

    1

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -4,11 +4,11 @@
   ...
 }: {
   stylix.enable = true;
-  stylix.base16Scheme = pkgs.base16-schemes.tokyo-night-moon;
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-moon.yaml";
   # stylix.image = ./wallpaper.jpg;
   stylix.cursor = {
     package = pkgs.bibata-cursors;
     name = "Bibata-Modern-Ice";
-    size = "20";
+    size = 20;
   };
 }
