4e217d247c56fae355808017c93be5d2c4a401a4
Alsesd <mr.alsesd@gmail.com>
2025-08-20 14:19:50 +0300

    1

--- modules/sddm.nix
+++ modules/sddm.nix
@@ -7,27 +7,27 @@
     # Let Stylix handle the theming, but you can still use a custom theme
     # Comment out the theme line to let Stylix theme SDDM
     theme = "pixel_sakura";
-
-    # Or install and use a custom theme package
-    #settings = {
-    #  Theme = {
-    # Current working directory for theme files
-    #   CursorTheme = "Bibata-Modern-Ice";
-    #   CursorSize = 24;
-
-    # Enable Qt theming
-    #    EnableAvatars = true;
-    #   UserPicture = true;
-    # };
-    # };
-    #};
-
-    services.xserver.enable = true;
-    environment.systemPackages = with pkgs; [
-      sddm-astronaut # Contains pixel_sakura theme
-      kdePackages.sddm-kcm # KDE configuration module for SDDM
-      libsForQt5.qt5.qtquickcontrols2
-      libsForQt5.qt5.qtgraphicaleffects
-    ];
   };
+
+  # Or install and use a custom theme package
+  #settings = {
+  #  Theme = {
+  # Current working directory for theme files
+  #   CursorTheme = "Bibata-Modern-Ice";
+  #   CursorSize = 24;
+
+  # Enable Qt theming
+  #    EnableAvatars = true;
+  #   UserPicture = true;
+  # };
+  # };
+  #};
+
+  services.xserver.enable = true;
+  environment.systemPackages = with pkgs; [
+    sddm-astronaut # Contains pixel_sakura theme
+    kdePackages.sddm-kcm # KDE configuration module for SDDM
+    libsForQt5.qt5.qtquickcontrols2
+    libsForQt5.qt5.qtgraphicaleffects
+  ];
 }
