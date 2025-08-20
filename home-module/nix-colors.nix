ebbf551cae1e342fcec9070dfb3c73ace0d1538b
Alsesd <mr.alsesd@gmail.com>
2025-08-20 13:18:07 +0300

    1

--- home-module/nix-colors.nix
+++ home-module/nix-colors.nix
@@ -10,9 +10,7 @@
 
   colorScheme = nix-colors.colorSchemes.dracula;
 
-  programs.sddm = {
-    enable = true;
-    theme = "dracula";
+
     programs.kitty = {
       enable = true;
       theme = "dracula";
