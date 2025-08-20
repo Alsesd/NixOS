1c40607b2396e2b096a03943845ffeb83e3c2606
Alsesd <mr.alsesd@gmail.com>
2025-08-20 14:27:17 +0300

    1

--- home.nix
+++ home.nix
@@ -9,12 +9,12 @@
     # inputs.stylix.homeManagerModules.stylix
   ];
 
-  stylix.targets = {
-    waybar.enable = false; # We have custom waybar config
-    kitty.enable = true;
-    rofi-wayland.enable = true;
-    vscode.enable = true;
-  };
+  #stylix.targets = {
+  #  waybar.enable = false; # We have custom waybar config
+  #  kitty.enable = true;
+  #  rofi-wayland.enable = true;
+  #  vscode.enable = true;
+  #};
 
   home.username = "alsesd";
   home.homeDirectory = "/home/alsesd";
