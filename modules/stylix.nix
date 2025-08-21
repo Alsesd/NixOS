13b20e1909e80b2280491b2ff6710dc8c48b4043
Alsesd <mr.alsesd@gmail.com>
2025-08-21 13:58:31 +0300

    stylix

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -3,44 +3,52 @@
   inputs,
   ...
 }: {
-  stylix.enable = true;
-  stylix.homeManagerIntegration = {
-    autoImport = true; # Automatically import Stylix settings into Home Manager
-    followSystem = true; # Follow system settings
-  };
-  #stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/atelier-lakeside.yaml";
-  stylix.image = ./wallpaper.png;
-  stylix.cursor = {
-    package = pkgs.bibata-cursors;
-    name = "Bibata-Modern-Ice";
-    size = 18;
-  };
-  stylix.fonts = {
-    monospace = {
-      package = pkgs.nerd-fonts.jetbrains-mono; # Fixed syntax
-      name = "JetBrainsMono Nerd Font Mono";
-    };
-    sansSerif = {
-      package = pkgs.dejavu_fonts;
-      name = "DejaVu Sans";
-    };
-    serif = {
-      package = pkgs.dejavu_fonts;
-      name = "DejaVu Serif";
-    };
-    sizes = {
-      applications = 12;
-      terminal = 15;
-      desktop = 11;
-      popups = 10;
-    };
-  };
+  stylix = {
+    enable = true;
 
-  # Opacity settings
-  stylix.opacity = {
-    applications = 0.75;
-    terminal = 0.7;
-    desktop = 1.0;
-    popups = 0.9;
+    homeManagerIntegration = {
+      autoImport = true;
+      followSystem = true;
+    };
+
+    #stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/atelier-lakeside.yaml";
+
+    image = ./wallpaper.png;
+
+    polarity = "dark";
+
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+      size = 18;
+    };
+
+    fonts = {
+      monospace = {
+        package = pkgs.nerd-fonts.jetbrains-mono; # Fixed syntax
+        name = "JetBrainsMono Nerd Font Mono";
+      };
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+      sizes = {
+        applications = 12;
+        terminal = 15;
+        desktop = 11;
+        popups = 10;
+      };
+    };
+
+    opacity = {
+      applications = 0.75;
+      terminal = 0.7;
+      desktop = 1.0;
+      popups = 0.9;
+    };
   };
 }
