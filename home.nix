62a400f2f48a88955e8a27ca023660ecb7d7eacf
Alsesd <mr.alsesd@gmail.com>
2025-08-21 06:32:10 +0300

    1

--- home.nix
+++ home.nix
@@ -6,13 +6,12 @@
 }: {
   imports = [
     ./home-module/waybar.nix
-    inputs.stylix.homeManagerModules.stylix
   ];
 
   #programs.hyprland.enable = true;
   programs.nixos-icons.enable = true;
   programs.waybar.enable = true;
-  programs.cava.rainbow.enable = true;
+  programs.cava-rainbow.enable = true;
   programs.rofi.enable = true;
   programs.kitty.enable = true;
   home.username = "alsesd";
