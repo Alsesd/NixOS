1722b72b87fb9bbd3eb61c3c447ee3a4965e5d76
Alsesd <mr.alsesd@gmail.com>
2025-08-21 06:30:01 +0300

    theme

--- home.nix
+++ home.nix
@@ -9,13 +9,13 @@
     inputs.stylix.homeManagerModules.stylix
   ];
 
-  stylix.targets.hyprland.enable = true;
-  stylix.targets.nixos-icons.enable = true;
-  programs.targets.waybar.enable = true;
-  programs.targets.cava.rainbow.enable = true;
-  programs.targets.rofi.enable = true;
-  programs.targets.kitty.enable = true;
-.username = "alsesd";
+  #programs.hyprland.enable = true;
+  programs.nixos-icons.enable = true;
+  programs.waybar.enable = true;
+  programs.cava.rainbow.enable = true;
+  programs.rofi.enable = true;
+  programs.kitty.enable = true;
+  home.username = "alsesd";
   home.homeDirectory = "/home/alsesd";
   home.stateVersion = "25.05";
 
