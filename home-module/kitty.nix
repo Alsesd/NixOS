2c53a7bd2c849ce2cb56810402c5bd466176ff65
Alsesd <mr.alsesd@gmail.com>
2025-08-21 08:27:56 +0300

    1

--- home-module/kitty.nix
+++ home-module/kitty.nix
@@ -18,7 +18,6 @@
       bold_font = "auto";
       italic_font = "auto";
       bold_italic_font = "auto";
-      background_opacity = "0.7";
       enable_audio_bell = false;
       confirm_os_windows_close = 0;
     };
