c07e3f2836b6c9e4737dc1b0708a208dac3937d6
Alsesd <mr.alsesd@gmail.com>
2025-09-06 17:46:58 +0300

    1

--- configuration.nix
+++ configuration.nix
@@ -71,4 +71,17 @@
     xfce.thunar
   ];
   system.stateVersion = "25.05"; # Did you read the comment?
+  environment.variables = {
+    GDK_BACKEND = "wayland,x11,*";
+    QT_QPA_PLATFORM = "wayland;xcb";
+    CLUTTER_BACKEND = "wayland";
+    XDG_CURRENT_DESKTOP = "niri";
+    XDG_SESSION_DESKTOP = "niri";
+    XDG_SESSION_TYPE = "wayland";
+    LIBVA_DRIVER_NAME = "nvidia";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    NVD_BACKEND = "direct";
+    GSK_RENDERER = "ngl";
+    GBM_BACKEND = "nvidia-drm";
+  };
 }
