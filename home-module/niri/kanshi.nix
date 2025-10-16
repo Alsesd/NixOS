fa6e3b80765200578faf0dc155a93f2302c4ca84
Alsesd <mr.alsesd@gmail.com>
2025-10-17 01:26:07 +0300

    1

--- home-module/niri/kanshi.nix
+++ home-module/niri/kanshi.nix
@@ -2,24 +2,41 @@
   home.packages = with pkgs; [
     kanshi
   ];
-  xdg.configFile."kanshi/config".text = ''    ;
+  xdg.configFile."kanshi/config".text = ''
 
-      profile base {
-    	  output HDMI-A-4 {
-        enable
-        mode 1920x1080@144
-        position 1920 0
-        scale 1.0
-        primary yes
-      }
-      output eDP-1 {
-        enable
-        mode 1920x1080@60
-        position 0 0
-        scale 1.0
-        primary no
-      }
+          profile base {
+        	  output HDMI-A-4 {
+            enable
+            mode 1920x1080@144
+            position 1920,0
+            scale 1.0
+          }
+          output eDP-1 {
+            enable
+            mode 1920x1080@60
+            position 0,0
+            scale 1.0
+          }
+          exec "exec niri msg output HDMI-A-4 on"
+    }
 
+    profile laptop {
+        	  output eDP-1 {
+            enable
+            mode 1920x1080@60
+            position 0,0
+            scale 1.0
+          }
+    }
   '';
-  #home.file.".config/kanshi/config".source = xdg.configFile."kanshi/config";
+  systemd.user.services.kanshi = {
+    description = "Turn on HDMI-A-4";
+    wantedBy = ["display-manager.service"];
+    partOf = ["systemd-bootctl.socket"];
+    #after = ["systemd-user-sessions.service "];
+    serviceConfig = {
+      Type = "exec";
+      ExecStart = "kanshi --load-profile base";
+    };
+  };
 }
