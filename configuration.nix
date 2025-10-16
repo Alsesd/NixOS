e6cd79ac52dd829f3b4079fcc6bf3917c160d4f4
Alsesd <mr.alsesd@gmail.com>
2025-10-16 23:06:42 +0300

    1

--- configuration.nix
+++ configuration.nix
@@ -25,7 +25,6 @@
   };
 
   programs.niri.enable = true;
-  services.displayManager.sddm.sugarCandyNix.enable = true;
   services.displayManager.sddm.wayland.enable = true;
   # Create a wrapper script for xdg-open to handle directories
   environment.systemPackages = with pkgs; [
